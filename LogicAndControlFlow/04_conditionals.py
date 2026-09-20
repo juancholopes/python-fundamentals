d141320726ccf53f8991e101bdc6e98830c42cc6
juancholopes <177896874+juancholopes@users.noreply.github.com>
2026-09-20 17:14:18 -0500

    Añadir clase de condicionales y match

---
+++ LogicAndControlFlow/04_conditionals.py
@@ -0,0 +1,16 @@
+x = 10
+z = 5
+if x > z:
+    print("x es mayor que z")
+elif z > 10:
+    print("x no es mayor que z")
+else:
+    pass
+
+
+# En el caso en el que la condicion no tenga una solución
+
+if z > 10:
+    pass
+else:
+    pass
---
+++ LogicAndControlFlow/05_match.py
@@ -0,0 +1,9 @@
+dia = "Lunes"
+
+match dia:
+    case "Lunes":
+        print("Hoy es lunes")
+    case "Martes":
+        print("Hoy es martes")
+    case _:
+        pass
