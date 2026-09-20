e71725c461b3fa06e0ff87719df0d828285e3953
juancholopes <177896874+juancholopes@users.noreply.github.com>
2026-09-20 17:15:16 -0500

    Add while and for loops

---
+++ LogicAndControlFlow/06_while.py
@@ -0,0 +1,8 @@
+x = 1
+
+while x <= 10:
+    if x % 2 == 0:
+        print(f"{x} es par")
+    elif x == 9:
+        break
+    x += 1
---
+++ LogicAndControlFlow/07_for.py
@@ -0,0 +1,8 @@
+i = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for i in i:
+    if i % 2 == 0:
+        print(f"{i} es par")
+    elif i % 2 != 0:
+        print(f"{i} es impar")
+
